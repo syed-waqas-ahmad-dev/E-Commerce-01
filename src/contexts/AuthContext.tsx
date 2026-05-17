bc78ccdd8846c35cf60b1e5d5d11b167aaa290aa
Syed Waqas Ahmed <ahmadsyedwaqas4@gmail.com>
2026-05-17 22:39:00 +0500

    Update AuthContext.tsx

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -124,7 +124,7 @@ const signInWithOAuth = async (provider: 'google') => {
     const { error } = await supabase.auth.signInWithOAuth({
       provider,
       options: {
-        redirectTo: `${window.location.origin}/account`,
+        redirectTo: `${window.location.origin}/auth/callback`,
         queryParams: {
           prompt: 'select_account consent',
         },
